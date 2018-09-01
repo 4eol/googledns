c0ebdb4c63be1971d5be9c724162029dca8d065c
4eol <42881481+4eol@users.noreply.github.com>
2018-09-01 09:28:45 +0800

    Update padavan.sh

--- dnsmasq/padavan.sh
+++ dnsmasq/padavan.sh
@@ -36,7 +36,7 @@ cat > /etc/storage/dnsmasq/dns/start.sh << EOF
 #!/bin/sh
 cd /etc/storage/dnsmasq/dns/conf
 echo "--- 下载dnsmasq规则"
-wget --no-check-certificate https://raw.githubusercontent.com/sy618/hosts/master/dnsmasq/dnsfq -O /etc/storage/dnsmasq/dns/dnsfq;sed -i "1 i\## Download：\$(date "+%Y-%m-%d %H:%M:%S")" /etc/storage/dnsmasq/dns/dnsfq
+wget --no-check-certificate https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/dnsmasq -O /etc/storage/dnsmasq/dns/dnsfq;sed -i "1 i\## Download：\$(date "+%Y-%m-%d %H:%M:%S")" /etc/storage/dnsmasq/dns/dnsfq
 wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/ip.conf -O ip.conf;sed -i "1 i\## Download：\$(date "+%Y-%m-%d %H:%M:%S")" ip.conf
 wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/dnsmasq/union.conf -O union.conf;sed -i "1 i\## Download：\$(date "+%Y-%m-%d %H:%M:%S")" union.conf
 
